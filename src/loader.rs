50ecdcf3cda7a55824f03b40164a22de5f74dd55
Vilém Zouhar <vilem.zouhar@gmail.com>
2020-11-24 22:20:41 +0100

    improve loader performance

--- src/loader.rs
+++ src/loader.rs
@@ -6,37 +6,36 @@ use std::string::String;
 pub struct Mapper {
     pub map_to: HashMap<String, usize>,
     pub map_from: HashMap<usize, String>,
+    // points after the last one
+    counter: usize,
 }
 
 impl Mapper {
-    pub fn new(data: &Vec<Sentence>, words: bool) -> Mapper {
-        let mut map_to = HashMap::<String, usize>::new();
-        let mut map_from = HashMap::<usize, String>::new();
-
-        let mut counter = 0;
-        for sent in data {
-            for (word, tag) in &sent.tokens {
-                let token = String::from(if words { word } else { tag });
-                if !map_to.contains_key(&token) {
-                    map_to.insert(String::from(&token), counter);
-                    map_from.insert(counter, token);
-                    counter += 1;
-                }
-            }
+    pub fn empty() -> Mapper {
+        Mapper {
+            map_to: HashMap::<String, usize>::new(),
+            map_from: HashMap::<usize, String>::new(),
+            counter: 0,
         }
-        return Mapper { map_to, map_from };
+    }
+
+    pub fn update(&mut self, tok: &str) -> usize {
+        if let Some(val) = self.map_to.get(tok) {
+            return *val;
+        } else {
+            self.map_to.insert(String::from(tok), self.counter);
+            self.map_from.insert(self.counter, String::from(tok));
+            self.counter += 1;
+            return self.counter - 1;
+        }
     }
 }
 
 pub struct Sentence {
-    pub tokens: Vec<(String, String)>,
-}
-
-pub struct SentenceR {
     pub tokens: Vec<(usize, usize)>,
 }
 
-impl SentenceR {
+impl Sentence {
     pub fn print(&self, mapper_w: &Mapper, mapper_t: &Mapper) {
         for x in &self.tokens {
             println!(
@@ -51,13 +50,16 @@ impl SentenceR {
 }
 
 pub struct Loader {
-    pub data_r: Vec<SentenceR>,
+    pub data: Vec<Sentence>,
     pub mapper_w: Mapper,
     pub mapper_t: Mapper,
 }
 
 impl Loader {
     pub fn load(path: &str) -> Result<Loader, &str> {
+        let mut mapper_w = Mapper::empty();
+        let mut mapper_t = Mapper::empty();
+
         if let Ok(mut file) = File::open(path) {
             let mut contents = String::new();
             let result = file.read_to_string(&mut contents);
@@ -72,33 +74,18 @@ impl Loader {
                     } else {
                         let vals = line.split("\t").collect::<Vec<&str>>();
                         sent.tokens.push((
-                            String::from(vals[0]),
+                            mapper_w.update(vals[0]),
                             if vals.len() == 1 {
-                                String::from("")
+                                0
                             } else {
-                                String::from(vals[1])
+                                mapper_t.update(vals[1])
                             },
                         ));
                     }
                 }
 
-                let mapper_w = Mapper::new(&data, true);
-                let mapper_t = Mapper::new(&data, false);
-
-                // TODO: This can be done already at loading time, giving a performance boost
-                let mut data_r = Vec::<SentenceR>::new();
-                for sent in &data {
-                    let mut sent_r = SentenceR { tokens: vec![] };
-                    for (word, tag) in &sent.tokens {
-                        let word_r = mapper_w.map_to.get(word).unwrap();
-                        let tag_r = mapper_t.map_to.get(tag).unwrap();
-                        sent_r.tokens.push((*word_r, *tag_r));
-                    }
-                    data_r.push(sent_r);
-                }
-
                 return Ok(Loader {
-                    data_r,
+                    data,
                     mapper_w,
                     mapper_t,
                 });
@@ -109,7 +96,7 @@ impl Loader {
     }
 
     pub fn print(&self) {
-        for x in &self.data_r {
+        for x in &self.data {
             x.print(&self.mapper_w, &self.mapper_t)
         }
     }
